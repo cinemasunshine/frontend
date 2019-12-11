fe6dc9eab080d50915c5e5cb5fe497b006888f34
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-12-11 16:10:44 +0900

    ### Added  - 4DX with screenX時も注意文言を追加

--- src/client/app/functions/purchase.ts
+++ src/client/app/functions/purchase.ts
@@ -1,3 +1,5 @@
+import { factory } from '@cinerino/api-javascript-client';
+
 /**
  * 券種金額取得
  */
@@ -6,4 +8,17 @@ export function getTicketPrice(offer: any) {
     // const spseatAdd1 = (ticketInfo.spseatAdd1 === undefined) ? 0 : ticketInfo.spseatAdd1;
     const spseatAdd2 = (ticketInfo.spseatAdd2 === undefined) ? 0 : ticketInfo.spseatAdd2;
     return (ticketInfo.salePrice + spseatAdd2);
+}
+
+/**
+ * 4DX判定
+ */
+export function is4DX(screeningEvent?: factory.chevre.event.screeningEvent.IEvent) {
+    if (screeningEvent === undefined
+        || screeningEvent.superEvent.coaInfo === undefined
+        || screeningEvent.superEvent.coaInfo.kbnJoueihousiki === undefined) {
+        return false;
+    }
+    const code = screeningEvent.superEvent.coaInfo.kbnJoueihousiki.kubunCode;
+    return (code === '002' || code === '004');
 }
