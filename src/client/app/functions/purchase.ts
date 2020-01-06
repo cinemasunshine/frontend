df45fea2ef4a3b350cc570827b157a6b8ae3c32a
Akito Hataguchi <hataguchi@motionpicture.jp>
2020-01-06 15:37:58 +0900

    ### Changed  - スケジュールをjson形式で取得へ変更

--- src/client/app/functions/purchase.ts
+++ src/client/app/functions/purchase.ts
@@ -1,4 +1,5 @@
 import { factory } from '@cinerino/api-javascript-client';
+import { IMovie, ISchedule, Performance } from '../models';
 
 /**
  * 券種金額取得
@@ -21,4 +22,41 @@ export function is4DX(screeningEvent?: factory.chevre.event.screeningEvent.IEven
     }
     const code = screeningEvent.superEvent.coaInfo.kbnJoueihousiki.kubunCode;
     return (code === '002' || code === '004');
+}
+
+/**
+ * スケジュールからパフォーマンスへ変換
+ */
+export function schedule2Performance(schedule: ISchedule, member: boolean) {
+    const performances: Performance[] = [];
+    const date = String(schedule.date);
+    schedule.movie.forEach((movie) => {
+        movie.screen.forEach((screen) => {
+            screen.time.forEach((time) => {
+                performances.push(new Performance({ date, movie, screen, time, member }));
+            });
+        });
+    });
+    return performances;
+}
+
+/**
+ * パフォーマンスを作品で絞り込み
+ */
+export function filterPerformancebyMovie(
+    performances: Performance[],
+    movie: IMovie) {
+    const filterResult =
+        performances.filter(p => {
+            return (p.movie.movie_short_code === movie.movie_short_code
+                && p.movie.movie_branch_code === movie.movie_branch_code);
+        });
+    const sortResult = filterResult.sort((a, b) => {
+        if (a.time.start_time < b.time.start_time) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return sortResult;
 }
