62709fe0cfc41152f2fac06191d083b4629fac32
Akito Hataguchi <hataguchi@motionpicture.jp>
2020-11-30 19:26:57 +0900

    ### Changed  - bootstrap適用

--- src/client/app/modules/shared/shared.module.ts
+++ src/client/app/modules/shared/shared.module.ts
@@ -4,13 +4,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BaseComponent } from './components/pages/base/base.component';
+import { AlertModalComponent } from './components/parts/alert-modal/alert-modal.component';
 import { ButtonsComponent } from './components/parts/buttons/buttons.component';
+import { ConfirmModalComponent } from './components/parts/confirm-modal/confirm-modal.component';
 import { FooterComponent } from './components/parts/footer/footer.component';
 import { HeaderComponent } from './components/parts/header/header.component';
 import { IconComponent } from './components/parts/icon/icon.component';
 import { LoadingComponent } from './components/parts/loading/loading.component';
 import { ModalComponent } from './components/parts/modal/modal.component';
-import { NextButtonComponent } from './components/parts/next-button/next-button.component';
 import { ScreenComponent } from './components/parts/screen/screen.component';
 import { SiteSealComponent } from './components/parts/site-seal/site-seal.component';
 import { DurationPipe, LibphonenumberFormatPipe, TimeFormatPipe } from './pipes';
@@ -23,9 +24,13 @@ const components = [
   IconComponent,
   LoadingComponent,
   ModalComponent,
-  NextButtonComponent,
   ScreenComponent,
-  SiteSealComponent
+  SiteSealComponent,
+];
+
+const entryComponents = [
+  AlertModalComponent,
+  ConfirmModalComponent,
 ];
 
 const pipes = [
@@ -37,8 +42,10 @@ const pipes = [
 @NgModule({
   declarations: [
     ...components,
+    ...entryComponents,
     ...pipes
   ],
+  entryComponents,
   imports: [
     CommonModule,
     RouterModule,
@@ -48,6 +55,7 @@ const pipes = [
   ],
   exports: [
     ...components,
+    ...entryComponents,
     ...pipes,
     ReactiveFormsModule,
     FormsModule,
